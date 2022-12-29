dc82a54772c8b1bcecef226e95eb127dfc071252
aggelospincha <46649035+aggelospincha@users.noreply.github.com>
2022-12-29 16:04:15 +0200

    Update Domino.sql

--- Domino.sql
+++ Domino.sql
@@ -1,9 +1,11 @@
 
 
-
+DROP TABLE IF EXISTS `lousi`;
 CREATE DATABASE lousi;
 
 
+
+DROP TABLE IF EXISTS `tile`;
 CREATE TABLE tile(
 `tilename` varchar (15) NOT NULL,
 `firstvalue` int NOT NULL,
@@ -12,52 +14,18 @@ primary key(tilename)
 
 );
 
-Insert into tile(tilename,firstvalue,secondvalue)values 
-('0-0',0,0),
 
-('0-1',0,1),
-('1-1',1,1),
-
-('0-2',0,2),
-('1-2',1,2),
-('2-2',2,2),
-
-('0-3',0,3),
-('1-3',1,3),
-('2-3',2,3),
-('3-3',3,3),
-
-('0-4',0,4),
-('1-4',1,4),
-('2-4',2,4),
-('3-4',3,4),
-('4-4',4,4),
-
-('0-5',0,5),
-('1-5',1,5),
-('2-5',2,5),
-('3-5',3,5),
-('4-5',4,5),
-('5-5',5,5);
-
-('0-6',0,6);
-('1-6',1,6);
-('2-6',2,6);
-('3-6',3,6);
-('4-6',4,6);
-('5-6',5,6);
-('6-6',6,6);
-
-
+DROP TABLE IF EXISTS `players`;
 CREATE TABLE players (
     `id` int NOT NULL AUTO_INCREMENT,
     `name` varchar(255) NOT NULL,
     /*`handtiles` varchar(10) ,*/
     `last_action` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-    PRIMARY KEY (id),
-    FOREIGN KEY (handtiles) References tile (tilename)
+    PRIMARY KEY (id)
+   /* FOREIGN KEY (handtiles) References tile (tilename)*/
 );
 
+DROP TABLE IF EXISTS `gameStatus`;
 Create table  gameStatus(
     `id` int NOT NULL AUTO_INCREMENT,
     `session_id` int NOT NULL,
@@ -72,14 +40,14 @@ Create table  gameStatus(
     `winner` enum('0', '1', '2', '3', '4') NOT NULL,
     `loser` enum('0', '1', '2', '3', '4') NOT NULL,
     `first_round` BOOLEAN NOT NULL DEFAULT TRUE,
-    `last_change` timestamp DEFAULT NOW(),
+    /*`last_change` timestamp DEFAULT NOW(),*/
     `last_change`  timestamp NULL DEFAULT current_timestamp() 
     ON UPDATE current_timestamp(),
     
     PRIMARY KEY (id)
 );
 
-
+DROP TABLE IF EXISTS `board`;
 CREATE TABLE board (
 	`tile` varchar(10),
 	`last_change`  timestamp NULL DEFAULT current_timestamp(), 
@@ -99,18 +67,14 @@ BEGIN
     declare player int;
     declare share int;
 	
-	    SELECT COUNT(*) INTO player_count FROM players;
+	    SELECT COUNT(*) INTO player FROM players;
         SELECT COUNT(*) INTO pile FROM piles;
-        SET share = pile / player_count;
+        SET share = pile / player;
         UPDATE piles
         SET piles = share;
 	
     END//
 DELIMITER ;
-
-
-
-
 
 
 
