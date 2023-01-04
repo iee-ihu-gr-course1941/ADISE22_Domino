5a5799e94c8c6b9829b6a2409a39c9d8e1308971
aggelospincha <46649035+aggelospincha@users.noreply.github.com>
2023-01-04 21:57:38 +0200

    Update Domino.sql

--- Domino.sql
+++ Domino.sql
@@ -1,138 +1,45 @@
-DROP DATABASE IF EXISTS `lousi`;
-CREATE DATABASE lousi;
-USE lousi;
+-- --------------------------------------------------------
+-- Διακομιστής:                  localhost
+-- Έκδοση διακομιστή:            10.4.27-MariaDB - mariadb.org binary distribution
+-- Λειτ. σύστημα διακομιστή:     Win64
+-- HeidiSQL Έκδοση:              12.3.0.6589
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `players`;
-CREATE TABLE players (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(10) NOT NULL,
-  /* `handtiles` varchar(10) DEFAULT NULL UNIQUE, */
-    `token` varchar(100) DEFAULT NULL,
-   `last_action` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(), 
-    PRIMARY KEY (id) 
-  /* CONSTRAINT FOREIGN KEY (handtiles) References tile (tilename) */
-  /* PRIMARY KEY (handtiles) */
-)ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-/*
-INSERT INTO players(name)VALUES
-	('vasilis1');
-	
-	SELECT count(DISTINCT  name)
-	FROM players ;
-	
-	SELECT * FROM players ;
-	
-	Select count(*) from tile;
-*/
+-- Dumping structure for πίνακας lousi.board
+CREATE TABLE IF NOT EXISTS `board` (
+  `bid` enum('1','2') NOT NULL,
+  `btile` varchar(10) DEFAULT NULL,
+  `last_change` timestamp NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`bid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+-- Dumping data for table lousi.board: ~2 rows (approximately)
+INSERT INTO `board` (`bid`, `btile`, `last_change`) VALUES
+	('1', NULL, '2023-01-04 19:29:07'),
+	('2', NULL, '2023-01-04 19:29:07');
 
+-- Dumping structure for πίνακας lousi.board_empty
+CREATE TABLE IF NOT EXISTS `board_empty` (
+  `bid` enum('1','2') NOT NULL,
+  `btile` varchar(10) DEFAULT NULL,
+  `last_change` timestamp NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`bid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+-- Dumping data for table lousi.board_empty: ~0 rows (approximately)
 
-DROP TABLE IF EXISTS `tile`;
-CREATE TABLE tile(
-`tilename` varchar (15) NOT NULL,
-`firstvalue` int NOT NULL,
-`secondvalue` int NOT NULL,
-primary key(tilename)
-/*FOREIGN KEY (playername) References players (name) */
-)ENGINE=InnoDB DEFAULT CHARSET=UTF8;
-
-
-DROP TABLE IF EXISTS `sharetile`;
-CREATE TABLE sharetile(
-`id` INT NOT NULL,
-`tile_name` varchar (15) ,
-`player_name` varchar(10) ,
-PRIMARY KEY (id)
-)ENGINE=InnoDB DEFAULT CHARSET=UTF8;
-
-
-/*INSERT INTO sharetile(id) VALUES
-(0),(1),(2),(3),(4),(5),(6),(7),(8),(9),(10),(11),(12),(13),(14),(15),(16),(17),(18),(19),(20),(21),(22),(23),(24),(25),(26),(27),(28);*/
-  
-INSERT INTO tile(tilename,firstvalue,secondvalue) VALUES 
-('0-0',0,0),
-('0-1',0,1),
-('1-1',1,1),
-('0-2',0,2),
-('1-2',1,2),
-('2-2',2,2),
-('0-3',0,3),
-('1-3',1,3),
-('2-3',2,3),
-('3-3',3,3),
-('0-4',0,4),
-('1-4',1,4),
-('2-4',2,4),
-('3-4',3,4),
-('4-4',4,4),
-('0-5',0,5),
-('1-5',1,5),
-('2-5',2,5),
-('3-5',3,5),
-('4-5',4,5),
-('5-5',5,5),
-('0-6',0,6),
-('1-6',1,6),
-('2-6',2,6),
-('3-6',3,6),
-('4-6',4,6),
-('5-6',5,6),
-('6-6',6,6);
-
-DROP TABLE IF EXISTS `gameStatus`;
-Create table  gameStatus(
-    `id` int NOT NULL AUTO_INCREMENT,
-   /* `session_id` int NOT NULL, */
-    `status` enum(
-        'initialized',
-        'started',
-        'ended',
-        'aborted'
-    ) NOT NULL DEFAULT 'initialized',
-    `p_turn` ENUM('1','2') Default NULL,
-   /* `n_players` int NOT NULL, */
-    `result` enum('1', '2') DEFAULT NULL,
-    /*`first_round` BOOLEAN NOT NULL DEFAULT TRUE,*/
-    /*`last_change` timestamp DEFAULT NOW(),*/
-    `last_change`  timestamp NULL DEFAULT current_timestamp() 
-    ON UPDATE current_timestamp(),
-    
-    PRIMARY KEY (id)
-)ENGINE=InnoDB DEFAULT CHARSET=UTF8;
-
-
-/*
-
-SELECT * FROM gamestatus;*/
-
-DROP TABLE IF EXISTS `board`;
-CREATE TABLE board (
-	`bid`  ENUM  ('1','2'),
-	`btile` varchar(10),
-	`last_change`  timestamp NULL DEFAULT current_timestamp(), 
-    primary key(bid)
-)ENGINE=InnoDB DEFAULT CHARSET=UTF8;
-
-INSERT INTO board(bid) VALUES ('1'),('2');
-#SELECT * FROM board
-
-DROP TABLE IF EXISTS `board_empty`;
-CREATE TABLE board_empty (
-	`bid` ENUM ('1','2'),
-	`btile` varchar(10),
-	`last_change`  timestamp NULL DEFAULT current_timestamp(), 
-    primary key(bid)
-)ENGINE=InnoDB DEFAULT CHARSET=UTF8;
-
-
-/*Some procedure to help us later*/
-
-/*We create one procedure that we can manage to resart a game to clean the board*/
-DROP procedure IF EXISTS `cleanboard`;
+-- Dumping structure for procedure lousi.cleanboard
 DELIMITER //
-CREATE PROCEDURE cleanboard()
+CREATE PROCEDURE `cleanboard`()
 BEGIN
 	DROP TABLE  IF EXISTS `board`;
 	CREATE TABLE board SELECT * FROM board_empty;
@@ -141,39 +48,111 @@ BEGIN
   		update `gamestatus` set `status`='not active', `p_turn`=null, `result`=null,  `last_change` =null;
 END//
 DELIMITER ;
-/*
-CALL cleanboard();
 
-SELECT * FROM gamestatus;
-SELECT * FROM board;
-*/
+-- Dumping structure for πίνακας lousi.gamestatus
+CREATE TABLE IF NOT EXISTS `gamestatus` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` enum('initialized','started','ended','aborted') NOT NULL DEFAULT 'initialized',
+  `p_turn` enum('1','2') DEFAULT NULL,
+  `result` enum('1','2') DEFAULT NULL,
+  `last_change` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
-DROP procedure IF EXISTS `tile_shuffle`;
+-- Dumping data for table lousi.gamestatus: ~0 rows (approximately)
+
+-- Dumping structure for πίνακας lousi.players
+CREATE TABLE IF NOT EXISTS `players` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) NOT NULL,
+  `token` varchar(100) DEFAULT NULL,
+  `last_action` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- Dumping data for table lousi.players: ~0 rows (approximately)
+
+-- Dumping structure for procedure lousi.play_tile
 DELIMITER //
-CREATE PROCEDURE tile_shuffle()
+CREATE PROCEDURE `play_tile`(IN p_id ENUM ('1','2'), IN tile_name VARCHAR(15))
+BEGIN
+  	UPDATE board
+  	SET
+	  btile=tile_name,
+	  last_change = NOW()
+  	where bid=p_id;
+  	
+  	UPDATE gamestatus
+  	SET  p_turn=p_id,
+  		status='started'
+  	WHERE id=1;
+  		 
+END//
+DELIMITER ;
+
+-- Dumping structure for πίνακας lousi.sharetile
+CREATE TABLE IF NOT EXISTS `sharetile` (
+  `id` int(11) NOT NULL,
+  `tile_name` varchar(15) DEFAULT NULL,
+  `player_name` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- Dumping data for table lousi.sharetile: ~0 rows (approximately)
+
+-- Dumping structure for πίνακας lousi.tile
+CREATE TABLE IF NOT EXISTS `tile` (
+  `tilename` varchar(15) NOT NULL,
+  `firstvalue` int(11) NOT NULL,
+  `secondvalue` int(11) NOT NULL,
+  PRIMARY KEY (`tilename`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- Dumping data for table lousi.tile: ~28 rows (approximately)
+INSERT INTO `tile` (`tilename`, `firstvalue`, `secondvalue`) VALUES
+	('0-0', 0, 0),
+	('0-1', 0, 1),
+	('0-2', 0, 2),
+	('0-3', 0, 3),
+	('0-4', 0, 4),
+	('0-5', 0, 5),
+	('0-6', 0, 6),
+	('1-1', 1, 1),
+	('1-2', 1, 2),
+	('1-3', 1, 3),
+	('1-4', 1, 4),
+	('1-5', 1, 5),
+	('1-6', 1, 6),
+	('2-2', 2, 2),
+	('2-3', 2, 3),
+	('2-4', 2, 4),
+	('2-5', 2, 5),
+	('2-6', 2, 6),
+	('3-3', 3, 3),
+	('3-4', 3, 4),
+	('3-5', 3, 5),
+	('3-6', 3, 6),
+	('4-4', 4, 4),
+	('4-5', 4, 5),
+	('4-6', 4, 6),
+	('5-5', 5, 5),
+	('5-6', 5, 6),
+	('6-6', 6, 6);
+
+-- Dumping structure for procedure lousi.tile_shuffle
+DELIMITER //
+CREATE PROCEDURE `tile_shuffle`()
 BEGIN
 	DROP TABLE IF EXISTS `clonetile`;
 		CREATE TABLE clonetile AS
 		SELECT * FROM tile			
 		ORDER BY RAND();				
 	END//
-
 DELIMITER ;
 
-
-/*
-SELECT * FROM tile;
-
-CALL tile_shuffle();
-/*
-/*
-# SELECT COUNT(*) FROM clonetile;
-# select * from clonetile;
-*/
-
-DROP procedure IF EXISTS `update_sharetile`;
+-- Dumping structure for procedure lousi.update_sharetile
 DELIMITER //
-CREATE PROCEDURE `update_sharetile` ()
+CREATE PROCEDURE `update_sharetile`()
 BEGIN
  	DECLARE counter INT;
   	SET counter=0;
@@ -207,72 +186,11 @@ BEGIN
   	SET player_name = (SELECT name FROM players ORDER BY RAND() LIMIT 1) DIV 
   	WHERE player_name IS NULL;*/
  
-END //
-DELIMITER ;
-
-/*
-
-CALL update_sharetile();
-
-SELECT * FROM sharetile;
-
-SELECT COUNT(*) FROM sharetile WHERE player_name='vasilis'; */
-
-/*DROP PROCEDURE IF EXISTS `play_tile`;
-DELIMITER//
-CREATE PROCEDURE `play_tile` ()
-BEGIN
-	 declare tile_name varchar;
-    UPDATE gameStatus
-    SET current_tile = tile_name;
 END//
-DELIMITER;*/
-
-/*
-SELECT * FROM sharetile; 
-select  count(DISTINCT tile_name) from sharetile;
-
-*/
-
-
-DROP procedure IF EXISTS `play_tile`;
-DELIMITER //
-CREATE PROCEDURE `play_tile` (IN p_id ENUM ('1','2'), IN tile_name VARCHAR(15))
-BEGIN
-  	UPDATE board
-  	SET
-	  btile=tile_name,
-	  last_change = NOW()
-  	where bid=p_id;
-  	
-  	UPDATE gamestatus
-  	SET  p_turn=p_id,
-  		status='started'
-  	WHERE id=1;
-  		 
-END //
 DELIMITER ;
-#last udate
-/*
-CALL cleanboard();
 
-CALL play_tile('1','6-3');
-
-
-SELECT * FROM board;
-
-SELECT * FROM tile;
-SELECT * FROM sharetile;
-
-SELECT * from gamestatus;
-
-*/
-
-
-
-
-
-
-
-
-
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
